ced0565575d3e715944f66d6c78bb0d90f814a0b
JubinThomas1997 <162334803+JubinThomas1997@users.noreply.github.com>
2025-11-21 17:41:25 +0100

    Fast Api sorted

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException,Path
+from fastapi import FastAPI, HTTPException,Path,Query
 import json
 
 app = FastAPI()
@@ -41,3 +41,19 @@ def view_patient(patient_id:str = Path(...,description= 'ID of the patient in th
 # in your API endpoints
 
 
+
+@app.get('/sort')
+def sort_patients(sort_by : str =Query(...,description= "Sort on the basisof height,weight or bmi"),order: str = Query( 'asc',description= 'sort in asc or desc order ')):
+    valid_fields = ['height','weight','bmi']
+
+    if sort_by not in valid_field:
+        raise HTTPException(status_code = 400 , detail =f'Invalid filed select from {valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail='Invalid order,select between asc or desc')
+    
+    data = load_data()
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values(), key= lambda x:x.get(sort_by,0),reverse=sort_order)
+
+    return sorted_data
